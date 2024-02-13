a6e612d2d6a0b4592b035d07074ef359c9093b0d
Bacze05 <147276014+Bacze05@users.noreply.github.com>
2024-02-13 20:51:34 -0300

    Correcion de modelos
    se corrigió modelos para poder ordenarlos a gusto

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< Updated upstream
+=======
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    orden= models.PositiveIntegerField(default=0 )
+
+    def __str__(self):
+        return self.nombre
+class Plato(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+>>>>>>> Stashed changes
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import *
+from django.views.generic import TemplateView,ListView,UpdateView, CreateView,DeleteView, View
 # Create your views here.
+class Categorias(LoginRequiredMixin, View):
+    model_categoria = Categoria
+    model_plato = Plato
+    template_name = 'menu/categoria/Categorias.html'
+
+    def get_queryset_categorias(self):
+        return self.model_categoria.objects.all().order_by('orden')  # Ordenar por el campo 'orden'
+
+    def get_queryset_platos(self):
+        return self.model_plato.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        categorias = self.get_queryset_categorias()
+        platos = self.get_queryset_platos()
+        context["categorias"] = categorias
+        context["platos"] = platos
+        return context
+    
+    def get(self, request, *args, **kwargs):  
+        return render(request, self.template_name, self.get_context_data())
