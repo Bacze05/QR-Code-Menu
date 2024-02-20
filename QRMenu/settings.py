cde771436d6f91f1a57b8864cf8183c9812b0153
Bacze05 <147276014+Bacze05@users.noreply.github.com>
2024-02-20 00:09:09 -0300

    staticfiles1.1

--- QRMenu/settings.py
+++ QRMenu/settings.py
@@ -146,7 +146,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
